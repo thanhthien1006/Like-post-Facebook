00d9dda631a0bb60489f496aef084b372ed2c03d
Truong Thanh Thien <18521431@gm.uit.edu.vn>
2021-05-23 23:22:58 +0700

    Add file

---
+++ Like_post_Facebook.py
@@ -0,0 +1,51 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+
+# chạy trình duyệt ẩn danh
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--incognito")
+
+browser = webdriver.Chrome(chrome_options=chrome_options)
+
+#độ lớn cửa sổ trình duyệt
+browser.maximize_window()
+
+#link dẫn tới trang web
+browser.get("https://vi-vn.facebook.com")
+
+# tìm id email và nhập email
+email = browser.find_element_by_id('email')
+email.send_keys("username@gmail.com")
+time.sleep(1)
+
+# tìm id pass và nhập pass
+passw = browser.find_element_by_id('pass')
+passw.send_keys("password ")
+time.sleep(1)
+
+# tìm name login và đăng nhập
+btnLogin = browser.find_element_by_name('login')
+btnLogin.send_keys(Keys.ENTER)
+time.sleep(1)
+
+browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+time.sleep(5)
+
+likes = browser.find_elements_by_xpath(
+    "//div[@class='tvfksri0 ozuftl9m']//div[@aria-label='Thích']")
+actions = ActionChains(browser)
+print(len(likes))
+time.sleep(5)
+
+for i in range(0, 5):
+    actions.move_to_element(likes[i]).perform()
+    browser.execute_script("arguments[0].click();", likes[i])
+    print(likes[i])
+    time.sleep(1)
+
+time.sleep(3)
+browser.close()
---
+++ README.md
@@ -0,0 +1,9 @@
+# Đăng nhập Facebook và like 5 bài đăng đầu tiên.
+## Các bước chạy Project.
+1. Clone Repository từ GitHub về máy Local.
+2. Mở Visual Studio Code.
+3. Vào thư mục vừa Clone Project về rồi kéo Folder vào Visual Studio Code.
+4. Mở file `Like_post_Facebook.py` sau đó sửa `username@gmail.com` và `password` thành username và password tài khoản Facebook mà bạn sử dụng.
+5. Mở Terminal trong Visual Studio Code và chạy file `Like_post_Facebook.py`.
+  
+	`Note: Không sử dụng tài khoản Facebook thật. Thay vào đó, hãy tạo một tài khoản Facebook ảo để tránh việc bị Facebook khóa tài khoản.`
